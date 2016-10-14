2376b1ad0c8e76a9da2e4464f6e62f53b6a89148
caromine <caromine7@gmail.com>
2016-10-14 13:00:20 -0500

    Moved files out of subdirectory.

---
+++ Notify.Droid.Test/obj/Debug/android/src/md5b6044c1f1b6dd78b64f9dffef042b931/MainActivity.java
@@ -0,0 +1,47 @@
+package md5b6044c1f1b6dd78b64f9dffef042b931;
+
+
+public class MainActivity
+	extends md5282f1122c1313907b9bf274dd2c2f344.FormsApplicationActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Notify.Droid.Test.MainActivity, Notify.Droid.Test, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Notify.Droid.Test.MainActivity, Notify.Droid.Test, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
